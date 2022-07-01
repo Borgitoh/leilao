eaade19bce6233857d368eb2a707d65dc41698ef
Kene Conde <kene.conde@jupiter.co.ao>
2022-07-01 22:14:57 +0100

    Novas tabelas no banco

--- leilao.sql
+++ leilao.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 26-Jun-2022 às 16:31
+-- Tempo de geração: 01-Jul-2022 às 21:09
 -- Versão do servidor: 5.7.36
 -- versão do PHP: 7.4.26
 
@@ -22,6 +22,47 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `leilao` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `leilao`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produto`
+--
+
+DROP TABLE IF EXISTS `produto`;
+CREATE TABLE IF NOT EXISTS `produto` (
+  `protocolo` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) DEFAULT NULL,
+  `descricao` varchar(255) DEFAULT NULL,
+  `dataInicial` varchar(255) DEFAULT NULL,
+  `dataFinal` varchar(255) DEFAULT NULL,
+  `situacao` int(11) NOT NULL,
+  `preco` double DEFAULT NULL,
+  PRIMARY KEY (`protocolo`),
+  KEY `situacao` (`situacao`)
+) ENGINE=MyISAM AUTO_INCREMENT=12346 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `situacao`
+--
+
+DROP TABLE IF EXISTS `situacao`;
+CREATE TABLE IF NOT EXISTS `situacao` (
+  `idSituacao` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`idSituacao`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `situacao`
+--
+
+INSERT INTO `situacao` (`idSituacao`, `nome`) VALUES
+(1, 'Gerada'),
+(2, 'Publicada'),
+(3, 'Expirada');
 
 -- --------------------------------------------------------
 
@@ -60,7 +101,7 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `IdTipoUsuario` int(11) DEFAULT NULL,
   PRIMARY KEY (`Idusario`),
   KEY `IdTipoUsuario` (`IdTipoUsuario`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `usuario`
@@ -68,7 +109,26 @@ CREATE TABLE IF NOT EXISTS `usuario` (
 
 INSERT INTO `usuario` (`Idusario`, `nome`, `email`, `senha`, `IdTipoUsuario`) VALUES
 (1, 'Kene', 'dumilde.matos@gmail.com', 'dumilde@123', 3),
-(2, 'ADMIN', 'admin@gmail.com', '123123aA', 1);
+(2, 'ADMIN', 'admin@gmail.com', '123123aA', 1),
+(5, 'Kene', 'teste@gmail.com', '123123', 3),
+(6, 'Kene', 'teste@gmail.com', '3131', 1),
+(7, 'Kene', 'teste@gmail.com', '123123', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `venda`
+--
+
+DROP TABLE IF EXISTS `venda`;
+CREATE TABLE IF NOT EXISTS `venda` (
+  `idVenda` int(11) NOT NULL AUTO_INCREMENT,
+  `produto` int(11) NOT NULL,
+  `usuario` int(11) NOT NULL,
+  PRIMARY KEY (`idVenda`),
+  KEY `produto` (`produto`),
+  KEY `usuario` (`usuario`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
